e366383ddafca9e9547bd8d971279b5445032fe8
Tripple-A <biodun9@gmail.com>
2020-05-01 14:49:35 +0100

    added Home.js file to components folder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.css';
+import Home from './Home'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
