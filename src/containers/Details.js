665d90ca1c0e4bada52ab38d20c50c4a8f791cde
Tripple-A <biodun9@gmail.com>
2020-05-12 22:46:02 +0100

    added more fields for detsiled info of selected course

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -7,9 +7,9 @@ const mapStateToProps = state => ({
 });
 
 const Detail = ({ courses, match }) => {
-    const addFav = () => {
-        alert('course added to favorites')
-    }
+  const addFav = () => {
+    alert('course added to favorites');
+  };
   useEffect(() => {
     async function wait() {
       await courses;
@@ -21,19 +21,50 @@ const Detail = ({ courses, match }) => {
     return (
       <div>
         <h5>{course[0].title}</h5>
-        <img src={course[0].imgUrl}/>
-        <p>Provider: {course[0].provider}</p>
-        <p>Author: {course[0].author}</p>
-        <p>Level: {course[0].level}</p>
-        <p>Medium: {course[0].medium}</p>
-        <p>Language: {course[0].language}</p>
-        <p>Category: {course[0].category}</p>
-        <p>Broader Category: {course[0].broad_category}</p>
-    <p>Short Description: {course[0].shortDescription.split('.')[0]}.</p>
-    <p>Ratings: {course[0].providerRatings}</p>
-    <p>Duration: {course[0].duration}</p>
-    <button>Add to Favorites</button>
-    <button type="button" onClick="addFav"><a href={course[0].url}>Take Course</a></button>
+        <img src={course[0].imgUrl} />
+        <p>
+          Provider:
+          {course[0].provider}
+        </p>
+        <p>
+          Author:
+          {course[0].author}
+        </p>
+        <p>
+          Level:
+          {course[0].level}
+        </p>
+        <p>
+          Medium:
+          {course[0].medium}
+        </p>
+        <p>
+          Language:
+          {course[0].language}
+        </p>
+        <p>
+          Category:
+          {course[0].category}
+        </p>
+        <p>
+          Broader Category:
+          {course[0].broad_category}
+        </p>
+        <p>
+          Short Description:
+          {course[0].shortDescription.split('.')[0]}
+          .
+        </p>
+        <p>
+          Ratings:
+          {course[0].providerRatings}
+        </p>
+        <p>
+          Duration:
+          {course[0].duration}
+        </p>
+        <button type="button" onClick={addFav}>Add to Favorites</button>
+        <button type="button"><a href={course[0].url}>Take Course</a></button>
       </div>
     );
   }
