99ef3ac3029ebc2d3446741b6ee8ad7fd908a658
Tripple-A <biodun9@gmail.com>
2020-05-12 22:39:55 +0100

    added favorite courses button

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -7,26 +7,42 @@ const mapStateToProps = state => ({
 });
 
 const Detail = ({ courses, match }) => {
-    console.log(courses);
-    useEffect(() => {
-        async function wait(){
-            await courses
-        }
-        wait()
-    }, [courses.length]);
+    const addFav = () => {
+        alert('course added to favorites')
+    }
+  useEffect(() => {
+    async function wait() {
+      await courses;
+    }
+    wait();
+  }, [courses.length]);
   const course = courses.filter(item => item.id === parseInt(match.params.id));
   if (courses.length > 0) {
-  return (
+    return (
       <div>
-    <h5>{course[0].title}</h5>
-    </div>
-  )}else{
-  return(
-      <div>
-       <h5>hello</h5>
-     
+        <h5>{course[0].title}</h5>
+        <img src={course[0].imgUrl}/>
+        <p>Provider: {course[0].provider}</p>
+        <p>Author: {course[0].author}</p>
+        <p>Level: {course[0].level}</p>
+        <p>Medium: {course[0].medium}</p>
+        <p>Language: {course[0].language}</p>
+        <p>Category: {course[0].category}</p>
+        <p>Broader Category: {course[0].broad_category}</p>
+    <p>Short Description: {course[0].shortDescription.split('.')[0]}.</p>
+    <p>Ratings: {course[0].providerRatings}</p>
+    <p>Duration: {course[0].duration}</p>
+    <button>Add to Favorites</button>
+    <button type="button" onClick="addFav"><a href={course[0].url}>Take Course</a></button>
       </div>
-  )}
+    );
+  }
+  return (
+    <div>
+      <h5>hello</h5>
+
+    </div>
+  );
 };
 
 export default connect(mapStateToProps)(Detail);
