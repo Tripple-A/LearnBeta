29ae93e47f4aee6f17f8f7c252d17b62b928ec13
Tripple-A <biodun9@gmail.com>
2020-05-19 10:09:27 +0100

    increased search button div

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -102,7 +102,7 @@ const Dashboard = ({
           {name}
         </h4>
         <br />
-        <button type="button" onClick={openNav}>Dashboard</button>
+        <button type="button" onClick={openNav} id="dash">Dashboard</button>
         <br />
         <Link to={`/favs/${name}`}>My favorites</Link>
         <br />
